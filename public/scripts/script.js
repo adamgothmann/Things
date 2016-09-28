d3a60826a06adff88cb3aa443ce635e5082b4c99
adamgothmann <adamgothmann@gmail.com>
2016-09-28 17:31:40 -0500

    can add things and pictures of them

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,5 +1,14 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', '$http', function($scope, $http){
-  console.log('wutttt');
+  $scope.array = [];
+  $scope.addThing = function(){
+    console.log('thing', $scope.thing);
+    $scope.test = {
+      thing: $scope.thing,
+      picture: $scope.picture
+    };
+    $scope.array.push($scope.test);
+    console.log($scope.array);
+  };
 }]);
